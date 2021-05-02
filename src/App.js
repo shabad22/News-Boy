620edbd952efe42c5a9c5dcef6a00c28da82a138
Manpreet Singh Lehal <48849565+shabad22@users.noreply.github.com>
2021-05-02 10:53:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -38,9 +38,6 @@ const App = () => {
 
     return (
         <div>
-//             <div className={classes.logoContainer} >
-//                 <img src={process.env.PUBLIC_URL + '/shogo.png'} className={classes.alanlogo} alt="NewsBoy!" />
-//             </div>
             <NewsCards articles={newsArticles} activeArticle={activeArticle}/>
         </div>
     )
